12453ec25be60727b3e4fe1ba65227213fa84f14
soup <68081409+oops408@users.noreply.github.com>
2026-03-13 23:55:56 -0700

    Added fields and constructor for standard Customer class.

---
+++ src/customers/Customer.java
@@ -0,0 +1,18 @@
+package customers;
+
+import cart.ShoppingCart;
+
+public abstract class Customer {
+
+    protected int customerId;
+    protected String firstName;
+    protected String lastName;
+    protected ShoppingCart cart;
+
+}
+
+public Customer(int customerId, String firstName, String lastName) {
+    this.customerId = customerId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
